d3d5e844151e13997179dc4edafa889459f51dab
Thais <tathyflor@gmail.com>
2023-04-26 18:07:28 -0300

    ,

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import validator from './validator.js';
 const btnCard = document.getElementById("btnValidate");
 const alertCard = document.getElementById("cardResult");
 btnCard.addEventListener('click', cardValidator);
+
 btnValidate.submit(function () {
     sendContactForm();
     return false;
@@ -15,7 +16,7 @@ function cardValidator() {
 
     if (creditCardNumber === "") {
         return alertCard.innerText = "Por favor, digite o número do seu cartão.";
-    } else if (creditCardNumber.length <= 13) {
+    } else if (creditCardNumber.length <= 16) {
         return alertCard.innerText = "Faltam alguns dígitos no número do seu cartão. Favor verificar.";
     } else if (finalResult === true) {
         return alertCard.innerText = "O seu cartão de número " + maskedNumbers + " é válido";
