8ce3f1895da408b27f99bac7148127d30efa7795
Thais <tathyflor@gmail.com>
2023-04-26 14:39:46 -0300

    mudanças

--- src/index.js
+++ src/index.js
@@ -1,4 +1,27 @@
 import validator from './validator.js';
 
+const btnCard = document.getElementById("btnValidate");
+const alertCard = document.getElementById("cardResult");
+btnCard.addEventListener('click', cardValidator);
+btnValidate.submit(function () {
+    sendContactForm();
+    return false;
+});
+
+function cardValidator() {
+    const creditCardNumber = document.getElementById("number").value;
+    const finalResult = validator.isValid(creditCardNumber);
+    const maskedNumbers = validator.maskify(creditCardNumber)
+
+    if (creditCardNumber === "") {
+        return alertCard.innerText = "Por favor, digite o número do seu cartão.";
+    } else if (creditCardNumber.length <= 13) {
+        return alertCard.innerText = "Faltam alguns dígitos no número do seu cartão. Favor verificar.";
+    } else if (finalResult === true) {
+        return alertCard.innerText = "O seu cartão de número " + maskedNumbers + " é válido";
+    } else {
+        return alertCard.innerText = "Infelizmente, o seu cartão de número " + maskedNumbers + " não está válido. Favor verificar.";
+    }
+}
 
 console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,3 +1,47 @@
 const validator = {
+
+    isValid(creditCardNumber) {
+        const newArray = [];
+        for (let i = 0; i < creditCardNumber.length; i++) {
+            const numCard = parseInt(creditCardNumber[i]);
+            newArray.push(numCard)
+        }
+
+        const cardNumReverse = newArray.reverse();
+        for (let i = 0; i < cardNumReverse.length; i++) {
+            if ((i + 1) % 2 === 0) {
+                cardNumReverse[i] = cardNumReverse[i] * 2;
+            }
+            if (cardNumReverse[i] >= 10) {
+                cardNumReverse[i] = cardNumReverse[i] - 9;
+            }
+        }
+
+
+        let finalSum = 0
+        for (let i = 0; i < cardNumReverse.length; i++) {
+            finalSum = finalSum + cardNumReverse[i]
+        }
+
+        if (finalSum % 10 === 0) {
+            return true
+        } else {
+            return false
+        }
+    },
+
+    maskify(creditCardNumber) {
+        const maskNumber = [];
+        for (let i = 0; i < creditCardNumber.length; i++) {
+            if (i < creditCardNumber.length - 4) {
+                maskNumber.push("#");
+            } else {
+                maskNumber.push(creditCardNumber[i])
+            }
+        }
+        const masked = maskNumber.join("");
+        return masked;
+    }
 }
+
 export default validator;
