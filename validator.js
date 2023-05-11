2b8482a9099f92407ddce8084d20a52897ae70f9
Thais Alves Pereira <tathyflor@gmail.com>
2023-05-11 13:29:41 -0300

    Updates

--- validator.js
+++ validator.js
@@ -1,24 +1,24 @@
 const validator = {
     
   isValid(CardNumber) {
-    const newArray = [];
+    const newArray = [];//array vazio para receber os dados que o usuário colocar
     for (let i = 0; i < CardNumber.length; i++) {
-      const numCard = parseInt(CardNumber[i]);
-      newArray.push(numCard)
+      const numCard = parseInt(CardNumber[i]);//Analisa a string e retorna um número, o que evita erros de concatenação
+      newArray.push(numCard)//O .push adiciona novos elementos ao array; ou seja, os números que serão recebidos no input serão alinhados aqui
     }
 
-    const cardNumReverse = newArray.reverse();
+    const cardNumReverse = newArray.reverse();//Uma nova variável receber a string, que será revertida pelo .reverse()
     for (let i = 0; i < cardNumReverse.length; i++) {
-      if ((i + 1) % 2 === 0) {
-        cardNumReverse[i] = cardNumReverse[i] * 2;
+      if ((i + 1) % 2 === 0) {//Esse i+1 vai dizer ao JS que ele precisa começar do segundo elemento
+        cardNumReverse[i] = cardNumReverse[i] * 2;//Agora ele vai pegar cada posição par (indicada pelo [i]) e vai multiplicar por 2
       }
       if (cardNumReverse[i] >= 10) {
-        cardNumReverse[i] = cardNumReverse[i] - 9;
+        cardNumReverse[i] = cardNumReverse[i] - 9;//Se o número for maior ou igual a 10, ele vai subtrair 9
       }
     }
 
     let finalSum = 0
-    for (let i = 0; i < cardNumReverse.length; i++) {
+    for (let i = 0; i < cardNumReverse.length; i++) {//reutiliza a const cardNumReverse que está "guardando" o array invertido
       finalSum = finalSum + cardNumReverse[i]
     }
 
@@ -30,12 +30,12 @@ const validator = {
   },
 
   maskify(CardNumber) {
-    const maskNumber = [];
+    const maskNumber = [];//Array vazio para receber os dados que não temos de antemão (usuário que vai informar)
     for (let i = 0; i < CardNumber.length; i++) {
-      if (i < CardNumber.length - 4) {
-        maskNumber.push("#");
+      if (i < CardNumber.length - 4) {//Isso vai "isolar" os 4 últimos números
+        maskNumber.push("#");//Vai substituir os números por # até chegar aos 4 dígitos finais
       } else {
-        maskNumber.push(CardNumber[i])
+        maskNumber.push(CardNumber[i])//Vai manter os números normais
       }
     }
     const masked = maskNumber.join("");
